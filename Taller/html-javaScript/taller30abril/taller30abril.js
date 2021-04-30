344f6550412eb4c55fad6c11fb400639a643882c
ramita-0 <ramiromoya321@gmail.com>
2021-04-30 14:43:53 -0300

    30abril inc

---
+++ Taller/html-javaScript/taller30abril/taller30abril.js
@@ -0,0 +1,20 @@
+var margen = "1cm";
+
+function botonIzquierda(){
+    var cuadrado = document.getElementById("cuadrado");
+    var nuevoValorDeMargenIzquierdo = parseInt(margen.charAt(0)) + 1;
+    margen = nuevoValorDeMargenIzquierdo + "cm";
+    cuadrado.style.marginLeft = newValue;
+}
+
+function botonDerecha(){
+
+}
+
+function hoverUp(){
+
+}
+
+function hoverDown(){
+
+}
