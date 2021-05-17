0b8c84b369d433ddc8397b0faf24fe57747b2004
alumno <alumno@ipm-5.lan>
2021-05-17 17:05:46 -0300

    asd

--- Laboratorio/Java/ej17mayo/src/com/company/Main.java
+++ Laboratorio/Java/ej17mayo/src/com/company/Main.java
@@ -5,20 +5,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner opcion = new Scanner(System.in);
         System.out.println("///// Bienvienido al Sistema de la Libreria ///// ");
-        while(true){
+        boolean check = true;
+        while(check == true){
             System.out.println("\n\n -Se vendio algun libro? y/n");
-            String yn = scanner.nextLine();
+            String yn = opcion.nextLine();
             if (yn.equals("y")){
+                Scanner ingreseNombre = new Scanner(System.in);
+                Scanner ingreseEditorial = new Scanner(System.in);
+                Scanner ingresePrecio = new Scanner(System.in);
                 System.out.println("Ingrese nombre, editorial y precio del libro");
-                String nombre = scanner.nextLine();
-                String editorial = scanner.nextLine();
-                float precio = scanner.nextFloat();
+                String nombre = ingreseNombre.nextLine();
+                String editorial = ingreseEditorial.nextLine();
+                float precio = ingresePrecio.nextFloat();
                 Libro libro = new Libro(nombre, editorial, precio);
             }
-            else if (yn.equals("n")) break;
-            else System.out.println("Respuesta incorrecta");
+            else check = false;
+
         }
     }
 }
