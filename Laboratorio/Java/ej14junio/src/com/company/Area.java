222a98ff573f2989744eeb6b95a1ca2509ef6fea
ramita-0 <ramiromoya321@gmail.com>
2021-06-25 11:12:26 -0300

    asd

--- Laboratorio/Java/ej14junio/src/com/company/Area.java
+++ Laboratorio/Java/ej14junio/src/com/company/Area.java
@@ -7,22 +7,19 @@ public class Area {
         double area = Math.PI * radio * radio;
         System.out.println(area);
     }
-
     static void areaEsfera(double radio){
       double area = 4 * Math.PI * radio * radio;
         System.out.println(area);
-    }
 
+    }
     static void areaCuadrado(double lado){
         double area = lado * lado;
         System.out.println(area);
     }
-
     static void areaCubo(double lado){
         double area = 6 * lado * lado;
         System.out.println(area);
     }
-
     static void areaTriangulo(double base, double altura){
         double area = (base * altura) / 2;
         System.out.println(area);
