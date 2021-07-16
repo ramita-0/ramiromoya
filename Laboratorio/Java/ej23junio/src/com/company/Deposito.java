2769cba252102b744e6a5328167b3a3d48a10380
ramita-0 <ramiromoya321@gmail.com>
2021-07-15 22:12:25 -0300

    ej23junio terminado, faltarian mejoras del UI nada mas

--- Laboratorio/Java/ej23junio/src/com/company/Deposito.java
+++ Laboratorio/Java/ej23junio/src/com/company/Deposito.java
@@ -5,24 +5,31 @@ import java.util.HashSet;
 
 public class Deposito {
     private HashMap<String,Integer> stock;
+    private HashMap<String,Float> precioStock;
 
     public Deposito(){
         this.stock = new HashMap<String,Integer>();
+        this.precioStock = new HashMap<String, Float>();
     }
 
     public HashMap<String, Integer> getStock() {
         return stock;
     }
+    public HashMap<String, Float> getPrecioStock() {
+        return precioStock;
+    }
 
     public void agregarActualizarProducto(Producto producto){
         if (!this.stock.containsKey(producto.getNombre())){
             this.stock.put(producto.getNombre(), producto.getCantidad());
+            this.precioStock.put(producto.getNombre(), producto.getPrecioUnitario());
         }
         else {
-            int stock = this.stock.get(producto.getNombre());
-            stock += producto.getCantidad();
+            int stockActual = this.stock.get(producto.getNombre());
+            stockActual += producto.getCantidad();
+            this.stock.put(producto.getNombre(), stockActual);
+            this.precioStock.put(producto.getNombre(), producto.getPrecioUnitario());
         }
-
     }
 
     public void printStock(){
@@ -30,41 +37,57 @@ public class Deposito {
             System.out.println(key + " " + this.stock.get(key));
         }
     }
-    public void printValorStock(HashSet<Producto> productos){
+
+    public void printValorStock(){
         float precioTotal = 0;
-        for (Producto producto : productos){
-            precioTotal += producto.getCantidad() * producto.getPrecioUnitario();
+        for (String key : this.stock.keySet()){
+            float cantidad = this.stock.get(key);
+            float precioUnidad = this.precioStock.get(key);
+            float multiplicacion = cantidad * precioUnidad;
+            precioTotal = precioTotal + multiplicacion;
         }
         System.out.println("\nValor del stock: $" + precioTotal);
     }
-    public void printProductosSinStock(HashSet<Producto> productos){
-        for (Producto producto : productos){
-            System.out.println("");
-            if (producto.getCantidad() == 0){
-                System.out.println(producto.getNombre());
+
+    public void printProductosSinStock(){
+        for ( String key : this.stock.keySet()){
+            if (this.stock.get(key) == 0){
+                System.out.println(key);
             }
         }
     }
-    public void printStockProducto(HashMap<String,Integer> stock, String nombreProducto){
-        if (stock.containsKey(nombreProducto)){
-            System.out.println(stock.get(nombreProducto));
+
+    public void printStockProducto(String nombreProducto){
+        if (this.stock.containsKey(nombreProducto)){
+            System.out.println(this.stock.get(nombreProducto) + " Unidades");
         }
         else {
             System.out.println("\nNo se encontro dicho producto. Intentelo de nuevo");
         }
     }
 
-    public void descontarStockProducto(HashSet<Producto> productos, String nombre, int cantidad){
-        System.out.println("\n Ingrese el nombre y cantidad del producto a descontar");
-        for (Producto producto : productos){
-            boolean found = false;
-            if (producto.getNombre().equals(nombre)){
-                producto.descontarStock(producto, cantidad);
-                found = true;
+    public void printPrecioProductos(){
+        for ( String key : this.precioStock.keySet()){
+            System.out.println(key + " " + this.precioStock.get(key));
+        }
+    }
+
+    public void printProductosConStockMenorAquince(){
+        for ( String key : this.stock.keySet()){
+            if ( this.stock.get(key) < 15){
+                System.out.println( key );
             }
-            if (!found){
-                System.out.println("\nNo se encontro dicho producto. Intentelo de nuevo");
-            }
+        }
+    }
+
+    public void descontarStockProducto(String nombre, int cantidad){
+        if (this.stock.containsKey(nombre)){
+            int stockActual = this.stock.get(nombre);
+            stockActual = stockActual - cantidad;
+            this.stock.put(nombre, stockActual);
+        }
+        else {
+            System.out.println("No existe ese producto en el deposito");
         }
     }
 }
--- Laboratorio/Java/ej23junio/src/com/company/Main.java
+++ Laboratorio/Java/ej23junio/src/com/company/Main.java
@@ -11,8 +11,8 @@ public class Main {
         Scanner nombreProducto = new Scanner(System.in);
         Scanner cantidadProducto = new Scanner(System.in);
         Scanner precioProducto = new Scanner(System.in);
-        HashSet<Producto> productos = new HashSet<Producto>();
         Deposito deposito = new Deposito();
+
         System.out.println("\nControl de stock de productos alimenticios");
         System.out.println("1- Ingresar producto");
         System.out.println(" 2- Descontar producto");
@@ -23,43 +23,57 @@ public class Main {
         System.out.println("      7- Mostrar stock de productos con <15 unidades");
         System.out.println("       0- exit");
 
-        //bug en el 4, fixear que se actualize bien el stock en el map
-
+        //faltan mejoras del UI
         while (true){
             int input = scanner.nextInt();
             if (input == 0){
                 break;
             }
+
             else if (input == 1){
                 System.out.println("\n Ingrese nombre, cantidad y precio del producto");
                 String nombre = nombreProducto.nextLine();
                 int cantidad = cantidadProducto.nextInt();
                 float precio = precioProducto.nextFloat();
                 Producto producto = new Producto(nombre, cantidad, precio);
-                productos.add(producto);
-                System.out.println("Producto añadido");
+                deposito.agregarActualizarProducto(producto);
             }
+
             else if (input == 2){
                 System.out.println("\n Ingrese el nombre y cantidad del producto a descontar");
                 String nombre = nombreProducto.nextLine();
                 int cantidad = cantidadProducto.nextInt();
-                deposito.descontarStockProducto(productos, nombre, cantidad);
+                deposito.descontarStockProducto(nombre, cantidad);
             }
+
             else if (input == 3){
                 deposito.printStock();
             }
+
             else if (input == 4){
-                deposito.printValorStock(productos);
-                System.out.println(productos);
-                System.out.println(deposito.getStock());
+                deposito.printValorStock();
             }
+
             else if (input == 5){
-                deposito.printProductosSinStock(productos);
+                deposito.printProductosSinStock();
             }
+
             else if (input == 6){
                 System.out.println("\n Ingrese el nombre del producto que quiere revisar");
                 String nombre = nombreProducto.nextLine();
-                deposito.printStockProducto(deposito.getStock(), nombre);
+                deposito.printStockProducto(nombre);
+            }
+
+            else if (input == 7){
+                deposito.printProductosConStockMenorAquince();
+            }
+
+            else if (input == 8){
+                deposito.printPrecioProductos();
+            }
+
+            else {
+                System.out.println("error");
             }
         }
     }
