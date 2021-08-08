1c13fab1fb79be437f9ed18cd25613b747372e82
ramita-0 <ramiromoya321@gmail.com>
2021-08-07 21:01:11 -0300

    Corregi la prueba, que ma habias pedido profe

--- Laboratorio/Java/examen28junio/src/com/company/Banco.java
+++ Laboratorio/Java/examen28junio/src/com/company/Banco.java
@@ -1,32 +1,68 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Banco {
     public String nombreBanco;
     public ArrayList<Cliente> visitas;
 
-    public Banco(String nombreBanco){
+    public Banco(String nombreBanco)
+    {
         this.nombreBanco = nombreBanco;
+        this.visitas = new ArrayList<>();
     }
 
     //le hice el metodo agregarVisitas para que el banco pueda ser creado antes o despues de haber ingresado personas, entonces
     //la persona puede crear el banco al principio, y mas adelante agregarle las visitas, para que no este obligada a hacerlo de entrada
-    public void agregarVisitas(ArrayList<Cliente> visitas){
-        this.visitas = visitas;
+    public void agregarVisitas(Cliente cliente)
+    {
+        this.visitas.add(cliente);
     }
 
-    public HashSet<Integer> cbusDeClientesVisitantes(ArrayList<Cliente> visitas){
-        HashSet<Integer> cbus = new HashSet<Integer>();
-        for(Cliente cliente : visitas){
+    public HashSet<Integer> cbusDeClientesVisitantes()
+    {
+        HashSet<Integer> cbus = new HashSet<>();
+
+        for (Cliente cliente : this.visitas)
+        {
             cbus.add(cliente.getCbu());
         }
+
         return cbus;
     }
-    public void printCbus(HashSet<Integer> cbus){
-        for(int i : cbus){
-            System.out.println(i);
+
+    public void printCbus()
+    {
+        System.out.println(this.cbusDeClientesVisitantes());
+    }
+
+    public HashMap<Integer,Integer> returnVisitasPorCbu()
+    {
+        HashMap<Integer,Integer> visitas = new HashMap<>();
+
+        for (Cliente cliente : this.visitas)
+        {
+            visitas.put(cliente.getCbu(), 0);
+        }
+
+        for (Cliente cliente : this.visitas)
+        {
+            int cbuActual = cliente.getCbu();
+            visitas.put(cbuActual, visitas.get(cbuActual) + 1);
+        }
+
+        return visitas;
+    }
+    public void printVisitasPorCbu()
+    {
+        HashMap<Integer,Integer> map = returnVisitasPorCbu();
+
+        for (Integer key : map.keySet())
+        {
+            System.out.println("CBU: " + key + "  Visitas: " + map.get(key));
         }
     }
 }
--- Laboratorio/Java/examen28junio/src/com/company/Main.java
+++ Laboratorio/Java/examen28junio/src/com/company/Main.java
@@ -11,17 +11,15 @@ public class Main {
         Cliente c3 = new Cliente("Marian", 11111, 6566665);
         Banco bancoRio = new Banco("Banco Rio");
 
-        ArrayList<Cliente> visitas = new ArrayList<>();
-        visitas.add(c1);
-        visitas.add(c2);
-        visitas.add(c3);
-        visitas.add(c1);
-        visitas.add(c3);
+        bancoRio.agregarVisitas(c1);
+        bancoRio.agregarVisitas(c2);
+        bancoRio.agregarVisitas(c3);
+        bancoRio.agregarVisitas(c1);
+        bancoRio.agregarVisitas(c2);
+        bancoRio.agregarVisitas(c1);
+        bancoRio.agregarVisitas(c1);
 
-
-        bancoRio.agregarVisitas(visitas);
-        HashSet<Integer> cbusBancoRio = new HashSet<>();
-        cbusBancoRio = bancoRio.cbusDeClientesVisitantes(visitas);
-        bancoRio.printCbus(cbusBancoRio);
+        bancoRio.printCbus();
+        bancoRio.printVisitasPorCbu();
     }
 }
