b18ef69ef40b2493ce2a0fb6ee012b10a879844c
francironee <francirone03@gmail.com>
2022-10-27 08:56:22 -0300

    avances ej

---
+++ 6to/tecnologias_de_soporte_informatico/ej4agosto/tp.py
@@ -0,0 +1,15 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    
+    pagina = requests.get("https://acharts.co/us_singles_top_100")
+
+    soup = BeautifulSoup(pagina.content, "html.parser")
+    table = soup.find("table", id="ChartTable")
+    for row in table.findAll("tr"):
+        position = row.find("span", {"itemprop": "position"})
+        print(position.prettify())
+
+        
