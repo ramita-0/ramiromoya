5fd9ca93f9967f814b5d0e700a13d3bf95642976
ramita-0 <ramiromoya321@gmail.com>
2022-11-10 08:35:41 -0300

    Terminado el ej4agosto

---
+++ 6to/tecnologias_de_soporte_informatico/ej4agosto/main.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup as soup
+import requests
+import pandas as pd
+
+nombre = []
+apellido = []
+tag = []
+circuito = []
+equipo = []
+tiempoVuelta = []
+y = []
+
+url = 'https://www.formula1.com/en/results.html/2021/fastest-laps.html'
+
+r = requests.get(url)
+s = soup(r.text, 'html.parser')
+
+y = y + [2022 for i in range(0,len(list(s.find_all('span', class_='hide-for-tablet'))))]
+nombre = nombre + list(x.text.strip() for x in (s.find_all('span', class_='hide-for-tablet')))
+apellido = apellido + list(x.text.strip() for x in (s.find_all('span', class_='hide-for-mobile')))
+tag = tag + list(x.text.strip() for x in (s.find_all('span', class_='uppercase hide-for-desktop')))
+circuito = circuito + list(x.text.strip() for x in (s.find_all('td', class_='width30 dark')))
+equipo = equipo + list(x.text.strip() for x in (s.find_all('td', class_='width25 semi-bold uppercase')))
+tiempoVuelta = tiempoVuelta + list(x.text.strip() for x in (s.find_all('td', class_='dark bold')))
+
+pd.set_option('display.max_columns', None)
+nombreCompleto = list(x + " " + z for x, z in zip(nombre, apellido))
+df = pd.DataFrame(zip(y, circuito, nombreCompleto, tag, equipo, tiempoVuelta),
+                  columns=['Anio', 'Circuito', 'Nombre', 'Tag', 'Equipo', 'Tiempo de Vuelta'])
+
+df.to_csv('./csv.csv')
