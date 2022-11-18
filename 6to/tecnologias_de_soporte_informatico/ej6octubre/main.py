21cb0270cd4644e82dca568273b774fbc4474311
ramita-0 <ramiromoya321@gmail.com>
2022-11-18 09:54:58 -0300

    terminado ej 6octubre

--- 6to/tecnologias_de_soporte_informatico/ej6octubre/main.py
+++ 6to/tecnologias_de_soporte_informatico/ej6octubre/main.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 nombres = pd.read_csv("./nombres-2010-2014.csv",sep=",",engine="python")
 actividades = pd.read_csv("./actividades-culturales-2021.csv",sep=";",engine="python")
 
+
+
+
+
 # Parte 1
 condicion = actividades.Modalidad == "Virtual"
 resultado = actividades[['Modalidad']][condicion].count().to_string().replace("Modalidad    ", "")
@@ -34,102 +38,98 @@ condicion = (actividades.descripcion_actividad.str.contains('musica')) & (activi
 resultado = actividades[['actividad']][condicion].count()
 print("1.8-", '\n', resultado, '\n')
 
+
+
+
+
 # Parte 2
 condicion = nombres.anio == 2012
-print(nombres[['nombre']][condicion].count())
+resultado = nombres[['nombre']][condicion].count()
+print("2.1-", '\n', resultado, '\n')
 
 condicion = (nombres.anio == 2012) & (nombres.nombre.str.startswith('S'))
-print(nombres[['nombre']][condicion].count())
+resultado = nombres[['nombre']][condicion].count()
+print("2.2-", '\n', resultado, '\n')
 
 condicion = (nombres.nombre.str.startswith('A')) | (nombres.nombre.str.startswith('E')) | (nombres.nombre.str.startswith('I')) | (nombres.nombre.str.startswith('O')) | (nombres.nombre.str.startswith('U'))
-print(nombres[['nombre']][condicion].count())
+resultadoVocales = nombres[['nombre']][condicion].count()
+print("2.3-", '\n', resultadoVocales, '\n')
 
 condicion = (nombres.nombre.str.startswith('A')) | (nombres.nombre.str.startswith('E')) | (nombres.nombre.str.startswith('I')) | (nombres.nombre.str.startswith('O')) | (nombres.nombre.str.startswith('U'))
 nombresVocal = nombres[['nombre']][condicion]
-
 condicion = (nombres.anio >= 2010) & (nombres.anio <= 2013)
-print(nombresVocal[['nombre']][condicion].count())
+resultado = nombresVocal[['nombre']][condicion].count()
+print("2.4-", '\n', resultado, '\n')
 
-numero = nombres.cantidad.max()
-condicion = nombres.cantidad == numero
-print(nombres[['nombre']][condicion])
+condicion = nombres.cantidad == nombres.cantidad.max()
+resultado = nombres[['nombre']][condicion]
+print("2.5-", '\n', resultado, '\n')
 
-condicion = nombres.anio == 2014
-anio2014 = nombres[condicion]
+anio2014 = nombres[nombres.anio == 2014]
+condicion = anio2014.cantidad == anio2014.cantidad.max()
+resultado = anio2014[['nombre']][condicion]
+print("2.6-", '\n', resultado, '\n')
 
-numero = anio2014.cantidad.max()
-condicion = anio2014.cantidad == numero
-print(anio2014[['nombre']][condicion])
+anio2010 = nombres[nombres.anio == 2010]
+condicion = anio2010.cantidad == anio2010.cantidad.min()
+resultado = anio2010[['nombre']][condicion]
+print("2.7-", '\n', resultado, '\n')
 
-condicion = nombres.anio == 2010
-anio2010 = nombres[condicion]
+anio2011 = nombres[(nombres.anio == 2011) & (nombres.nombre.str.startswith('A'))]
+condicion = anio2011.cantidad == anio2011.cantidad.min()
+resultado = anio2011[['nombre']][condicion]
+print("2.8-", '\n', resultado, '\n')
 
-numero = anio2010.cantidad.min()
-condicion = anio2010.cantidad == numero
-print(anio2010[['nombre']][condicion])
+resultado = nombres.cantidad.mean()
+print("2.9-", '\n', resultado, '\n')
 
-condicion = (nombres.anio == 2011) & (nombres.nombre.str.startswith('A'))
-anio2011 = nombres[condicion]
+print("2.10-", '\n')
+anio = nombres[nombres.anio == 2010]
+resultado = anio.cantidad.mean()
+print("2010", '\n', resultado, '\n')
 
-numero = anio2011.cantidad.min()
-condicion = anio2011.cantidad == numero
-print(anio2011[['nombre']][condicion])
+anio = nombres[nombres.anio == 2011]
+resultado = anio.cantidad.mean()
+print("2011", '\n', resultado, '\n')
 
-print(nombres.cantidad.mean())
+anio = nombres[nombres.anio == 2012]
+resultado = anio.cantidad.mean()
+print("2012", '\n', resultado, '\n')
 
-condicion = nombres.anio == 2010
-anio = nombres[condicion]
+anio = nombres[nombres.anio == 2013]
+resultado = anio.cantidad.mean()
+print("2013", '\n', resultado, '\n')
 
-print("Promedio de nombres distintos usados en el 2010")
-print(anio.cantidad.mean())
+anio = nombres[nombres.anio == 2014]
+resultado = anio.cantidad.mean()
+print("2014", '\n', resultado, '\n')
 
-condicion = nombres.anio == 2011
-anio = nombres[condicion]
+resultado = nombres.nombre.drop_duplicates().count()
+print("2.11-", '\n', resultado, '\n')
 
-print("Promedio de nombres distintos usados en el 2011")
-print(anio.cantidad.mean())
-
-condicion = nombres.anio == 2012
-anio = nombres[condicion]
-
-print("Promedio de nombres distintos usados en el 2012")
-print(anio.cantidad.mean())
-
-condicion = nombres.anio == 2013
-anio = nombres[condicion]
-
-print("Promedio de nombres distintos usados en el 2013")
-print(anio.cantidad.mean())
-
-
-condicion = nombres.anio == 2014
-anio = nombres[condicion]
-
-print("Promedio de nombres distintos usados en el 2014")
-print(anio.cantidad.mean())
-
-nombresUnicos = nombres.nombre.drop_duplicates()
-print(nombresUnicos.count())
-
+print("2.12-", '\n')
 condicion = nombres.nombre.str.startswith('A')
-print("Cantidad de nombres que empiezan con A")
-print(nombres[['nombre']][condicion].count())
+resultado = nombres[['nombre']][condicion].count()
+print("A-", '\n', resultado, '\n')
 
 condicion = nombres.nombre.str.startswith('E')
-print("Cantidad de nombres que empiezan con E")
-print(nombres[['nombre']][condicion].count())
+resultado = nombres[['nombre']][condicion].count()
+print("E-", '\n', resultado, '\n')
 
 condicion = nombres.nombre.str.startswith('I')
-print("Cantidad de nombres que empiezan con I")
-print(nombres[['nombre']][condicion].count())
+resultado = nombres[['nombre']][condicion].count()
+print("I-", '\n', resultado, '\n')
 
 condicion = nombres.nombre.str.startswith('O')
-print("Cantidad de nombres que empiezan con O")
-print(nombres[['nombre']][condicion].count())
+resultado = nombres[['nombre']][condicion].count()
+print("O-", '\n', resultado, '\n')
 
 condicion = nombres.nombre.str.startswith('U')
-print("Cantidad de nombres que empiezan con U")
-print(nombres[['nombre']][condicion].count())
+resultado = nombres[['nombre']][condicion].count()
+print("U-", '\n', resultado, '\n')
+
+
+
 
 
 # Grafico de torta
